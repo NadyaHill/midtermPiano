75b6d99b63b1c375ed7df8d8d8ceb5599256efca
Nadya <Hill@Nadyas-MacBook-Pro.local>
2016-02-14 18:10:20 -0700

    pianoMidterm

---
+++ piano.js
@@ -0,0 +1,108 @@
+angular.module("pianoApp", [])
+
+angular.module("pianoApp")
+    .controller("pianoController", ["$scope", function($scope) {
+
+
+
+        var piano = new Wad(Wad.presets.piano)
+        $scope.play = function(note){
+            piano.play({pitch:note})
+        }
+
+
+
+// connects the keyboard keys to notes on the piano
+
+
+
+        $scope.keyboardPlay = function(event, down){
+        var name;
+        var pitch;
+            switch (event.keyCode) {
+
+                case 68:
+                    name = "#C4";
+                    pitch = 'C4';
+                    break;
+
+
+                case 82:
+                    name = "#cSharp";
+                    pitch = 'C#4';
+                    break;
+
+                case 70:
+                    name = "#D4";
+                    pitch = 'D4';
+                    break;
+
+                case 84:
+                    name = "#dSharp"
+                    pitch = 'D#4';
+                    break;
+
+                case 71:
+                    name = "#E4";
+                    pitch = 'E4';
+                    break;
+
+                case 72:
+                    name = "#F4";
+                    pitch = 'F4';
+                    break;
+
+                case 85:
+                    name = "#fSharp";
+                    pitch = 'F#4';
+                    break;
+
+                case 74:
+                    name = "#G4";
+                    pitch = 'G4';
+                    break;
+
+                case 73:
+                    name = "#gSharp";
+                    pitch = 'G#4';
+                    break;
+
+                case 75:
+                    name = "#A4";
+                    pitch = 'A4';
+                    break;
+
+                case 79:
+                    name = "#aSharp";
+                    pitch = 'A#4';
+                    break;
+
+                case 76:
+                    name = "#B4";
+                    pitch = 'B4';
+                    break;
+
+                case 186:
+                    name = "#C5";
+                    pitch = 'C5';
+                    break;
+
+                // default:
+                //     $scope.snare()
+                //     break;
+
+            }
+                    if(down) {
+                        $(name).addClass("colorChange")
+                        $scope.play(pitch)
+                    }
+                    else {
+                        $(name).removeClass("colorChange")
+                    }
+        }
+
+    }])
+
+
+
+
